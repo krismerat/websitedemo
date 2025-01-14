abf4b41523600a0d0ba3774d58090b3c51b638c2
krismerat <krisruudmerat@gmail.com>
2025-01-14 12:43:06 -0500

    Update cv.js

--- cv.js
+++ cv.js
@@ -2,31 +2,39 @@ document.getElementById("proj0").onmouseover = function() {mouseOver()};
 document.getElementById("proj0").onmouseout = function() {mouseOut()};
 
 function mouseOver() {
-  document.getElementById("proj0").style.borderColor = "red";
+  document.getElementById("proj0").style.borderColor = "white";
+  document.getElementById("proj0").style.backgroundColor = "lightgrey";
+
 }
 
 function mouseOut() {
   document.getElementById("proj0").style.borderColor = "black";
+  document.getElementById("proj0").style.backgroundColor = "initial";
 }
 
 document.getElementById("proj1").onmouseover = function() {mouseOver1()};
 document.getElementById("proj1").onmouseout = function() {mouseOut1()};
 
 function mouseOver1() {
-  document.getElementById("proj1").style.borderColor = "red";
+  document.getElementById("proj1").style.borderColor = "white";
+  document.getElementById("proj1").style.backgroundColor = "lightgrey";
 }
 
 function mouseOut1() {
   document.getElementById("proj1").style.borderColor = "black";
+  document.getElementById("proj1").style.backgroundColor = "initial";
 }
 
 document.getElementById("proj2").onmouseover = function() {mouseOver1()};
 document.getElementById("proj2").onmouseout = function() {mouseOut1()};
 
 function mouseOver2() {
-  document.getElementById("proj2").style.borderColor = "red";
+  document.getElementById("proj2").style.borderColor = "white";
+  document.getElementById("proj2").style.backgroundColor = "lightgrey";
 }
 
 function mouseOut2() {
   document.getElementById("proj2").style.borderColor = "black";
+  document.getElementById("proj2").style.backgroundColor = "initial";
 }
+
